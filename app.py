4739bf63e839e6fb865640ca17d6f3c3d4e7a98a
Robin Ward <rcw0024@auburn.edu>
2022-02-27 12:18:37 -0600

    added feature to change node size

--- app.py
+++ app.py
@@ -44,22 +44,27 @@ def index():
                                layer1=data['layer1total'],
                                layer1color=data['layer1color'],
                                layer1visible=layer1display,
+                               layer1size=data['layer1size'],
 
                                layer2=data['layer2total'],
                                layer2color=data['layer2color'],
                                layer2visible=layer2display,
+                               layer2size=data['layer2size'],
 
                                layer3=data['layer3total'],
                                layer3color=data['layer3color'],
                                layer3visible=layer3display,
+                               layer3size=data['layer3size'],
 
                                layer4=data['layer4total'],
                                layer4color=data['layer4color'],
                                layer4visible=layer4display,
+                               layer4size=data['layer4size'],
 
                                layer5=data['layer5total'],
                                layer5color=data['layer5color'],
-                               layer5visible=layer5display, )
+                               layer5visible=layer5display,
+                               layer5size=data['layer5size'])
     except Exception as e:
         print(e)
         return render_template('index.html')
@@ -88,6 +93,12 @@ def createjson():
     layer3color = request.form.get("colorInput3")
     layer4color = request.form.get("colorInput4")
     layer5color = request.form.get("colorInput5")
+
+    layer1size = request.form.get("layer1size")
+    layer2size = request.form.get("layer2size")
+    layer3size = request.form.get("layer3size")
+    layer4size = request.form.get("layer4size")
+    layer5size = request.form.get("layer5size")
 
     layer1visibled3 = request.form.get("nodeDisplay1")
     layer2visibled3 = request.form.get("nodeDisplay2")
@@ -130,6 +141,12 @@ def createjson():
     layer4nodesint = int(layer4nodes)
     layer5nodesint = int(layer5nodes)
 
+    layer1sizeint = int(layer1size)
+    layer2sizeint = int(layer2size)
+    layer3sizeint = int(layer3size)
+    layer4sizeint = int(layer4size)
+    layer5sizeint = int(layer5size)
+
     xint = int(x)
     yint = int(y)
     zint = int(z)
@@ -139,52 +156,57 @@ def createjson():
          'z': zint,
          'layer1total': layer1nodesint,
          'layer1color': layer1color,
+         'layer1size': layer1sizeint,
          'layer1visible': layer1visibled3,
          'layer2total': layer2nodesint,
          'layer2color': layer2color,
+         'layer2size': layer2sizeint,
          'layer2visible': layer2visibled3,
          'layer3total': layer3nodesint,
          'layer3color': layer3color,
+         'layer3size': layer3sizeint,
          'layer3visible': layer3visibled3,
          'layer4total': layer4nodesint,
          'layer4color': layer4color,
+         'layer4size': layer4sizeint,
          'layer4visible': layer4visibled3,
          'layer5total': layer5nodesint,
          'layer5color': layer5color,
+         'layer5size': layer5sizeint,
          'layer5visible': layer5visibled3,
          'nodes': [],
          'links': []}
 
     for i in range(layer1nodesint):
-        d['nodes'].append({'id': i, 'group': 1, 'size': 4, 'fixed': True,
+        d['nodes'].append({'id': i, 'group': 1, 'size': layer1sizeint, 'fixed': True,
                            'x': random.randrange(0, xint),
                            'y': random.randrange(0, yint),
                            'z': random.randrange(0, zint),
                            'color': layer1color,
                            'visibility': layer1visible})
     for i in range(layer2nodesint):
-        d['nodes'].append({'id': i, 'group': 2, 'size': 8, 'fixed': True,
+        d['nodes'].append({'id': i, 'group': 2, 'size': layer2sizeint, 'fixed': True,
                            'x': random.randrange(0, xint),
                            'y': random.randrange(0, yint),
                            'z': random.randrange(0, zint),
                            'color': layer2color,
                            'visibility': layer2visible})
     for i in range(layer3nodesint):
-        d['nodes'].append({'id': i, 'group': 3, 'size': 10, 'fixed': True,
+        d['nodes'].append({'id': i, 'group': 3, 'size': layer3sizeint, 'fixed': True,
                            'x': random.randrange(0, xint),
                            'y': random.randrange(0, yint),
                            'z': random.randrange(0, zint),
                            'color': layer3color,
                            'visibility': layer3visible})
     for i in range(layer4nodesint):
-        d['nodes'].append({'id': i, 'group': 4, 'size': 12, 'fixed': True,
+        d['nodes'].append({'id': i, 'group': 4, 'size': layer4sizeint, 'fixed': True,
                            'x': random.randrange(0, xint),
                            'y': random.randrange(0, yint),
                            'z': random.randrange(0, zint),
                            'color': layer4color,
                            'visibility': layer4visible})
     for i in range(layer5nodesint):
-        d['nodes'].append({'id': i, 'group': 5, 'size': 20, 'fixed': True,
+        d['nodes'].append({'id': i, 'group': 5, 'size': layer5sizeint, 'fixed': True,
                            'x': random.randrange(0, xint),
                            'y': random.randrange(0, yint),
                            'z': random.randrange(0, zint),
