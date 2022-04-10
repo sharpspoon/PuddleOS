031ee64950bec67e56a87575505e0c2ebc7e897c
Matthew Freestone <matthewfreestone1593@gmail.com>
2022-04-10 15:24:30 -0500

    demonstrated insert and find

--- dbtests.py
+++ dbtests.py
@@ -1,3 +1,4 @@
+from random import randint
 from pymongo import MongoClient
 from pprint import pprint
 import os
@@ -6,17 +7,20 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
     MONGO_URL = os.getenv('MONGO_URL')
-    print(MONGO_URL)
     client = MongoClient(MONGO_URL, serverSelectionTimeoutMS=5000)
-
-    
+    db = client.test
     try:
-        pprint(client.server_info())
+        print("Successfully connected to MongoDB")
     except Exception:
         print("Unable to connect to the server.")
-    # db = client.admin
-    # serverStatusResult=db.command("serverStatus")
-    # pprint(serverStatusResult)
+
+    tosave = { "name": "MongoDB", "type": "database", "count": 1, "info": { "x": randint(1,10), "y": randint(1,10)} }
+    db.test.insert_one(tosave)
+    print("Inserted the document into the collection")
+
+    cursor = db.test.find()
+    for doc in cursor:
+        pprint(doc)
 
 if __name__ == "__main__":
     main()
